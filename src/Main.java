44fa5b08a9482012f16901481988846d5a02f02a
Alexander <kuhar@mail.ru>
2022-08-06 00:38:48 +0300

    Выполнил ДЗ номер 6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,72 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // task 1
+        int[] ints = new int[3];
+        for (int i = 0; i < ints.length; i++) {
+            ints[i] = i + 1;
+        }
+        ints[0] = 1;
+        ints[1] = 2;
+        ints[2] = 3;
+        double[] doubles = {1.57, 7.654, 9.986};
+        boolean[] booleans = {true, false};
+
+        //task 2
+        for (int i = 0; i < ints.length; i++) {
+            System.out.print(ints[i]);
+            if (i != ints.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < doubles.length; i++) {
+            System.out.print(doubles[i]);
+            if (i != doubles.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < booleans.length; i++) {
+            System.out.print(booleans[i]);
+            if (i != booleans.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+
+        //task 3
+        for (int i = ints.length - 1; i >= 0; i--) {
+            System.out.print(ints[i]);
+            if (i != 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = doubles.length - 1; i >= 0; i--) {
+            System.out.print(doubles[i]);
+            if (i != 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i =booleans.length-1; i >=0; i--) {
+            System.out.print(booleans[i]);
+            if (i != 0) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+
+        //task 4
+        for (int i =0; i< ints.length; i++) {
+            if (ints[i]% 2==1) {
+                ints[i]++;
+            }
+             System.out.print(ints[i]);
+            if (i != ints.length-1) {
+                System.out.print(",");
+            }
+    }
     }
 }
